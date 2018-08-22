83974ff258c516bfe2aaf09b2e38fb0e453ee6e2
tuck1s <steve.tuck@sparkpost.com>
2018-08-22 19:36:09 +0100

    Check for Splunk response (400) to empty requests and map to 200OK

--- lambda/main.go
+++ lambda/main.go
@@ -15,13 +15,14 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"net/http"
+	"strings"
 )
 
 // Lambda function handler
 func Handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
 	// stdout and stderr are sent to AWS CloudWatch Logs
-	log.Printf("%s %s %s\n", req.HTTPMethod, req.Path, req.RequestContext.RequestID)
+	log.Printf("Incoming: %s %s body %d bytes from SourceIP %s\n", req.HTTPMethod, req.Path, len(req.Body), req.RequestContext.Identity.SourceIP)
 
 	switch req.HTTPMethod {
 	case "POST":
@@ -36,23 +37,25 @@ func Handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse,
 
 // For marshalling Splunk events
 type splunkEvent struct {
-	time   int                    `json:"time"`
-	host   string                 `json:"host"`
-	source string                 `json:"source"`
-	event  map[string]interface{} `json:"event"`
+	Time   int                    `json:"time"`
+	Host   string                 `json:"host"`
+	Source string                 `json:"source"`
+	Event  map[string]interface{} `json:"event"`
+}
+
+type splunkResult struct {
+	Code int    `json:"code"`
+	Text string `json:"text"`
 }
 
 // Handler for an incoming webhooks POST. Make outgoing request
 func rest_webhooks_post(session events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	//const splunkUrl = "https://input-prd-p-52cm87k9p7sx.cloud.splunk.com:8088/services/collector"
-	const splunkUrl = "https://bigger-bin.herokuapp.com/1btyn7r1"
-
-	var buf = bytes.NewBufferString(session.Body)
 	// Received Webhook data is an array [].msys.xxx_event_key.event
 	var sparkPostWebhookEvents []map[string]map[string]map[string]interface{}
 	if err := json.Unmarshal([]byte(session.Body), &sparkPostWebhookEvents); err != nil {
 		log.Fatal(err)
 	}
+	var splunkOutputLines string
 	// Walk the contents, building a Splunk-compatible output
 	for _, ev := range sparkPostWebhookEvents {
 		// dereference msys.xxx_event_key, because "type" attribute is all we need to identify the event
@@ -62,25 +65,28 @@ func rest_webhooks_post(session events.APIGatewayProxyRequest) (events.APIGatewa
 			if err != nil {
 				log.Fatalf("Timestamp conversion error %s", err)
 			}
-			se.time = ts
-			se.host = "SparkPost" //TODO: replace with session IP adddr
-			se.source = "SparkPost"
-			se.event = event
+			se.Time = ts
+			se.Host = session.RequestContext.Identity.SourceIP
+			se.Source = "SparkPost"
+			se.Event = event
 			jsonb, err := json.Marshal(se)
 			if err != nil {
 				log.Fatalf("JSON marshaling failed : %s", err)
 			}
-			fmt.Println(string(jsonb))
+			splunkOutputLines += string(jsonb) + "\n"
 		}
 	}
+
+	var buf = bytes.NewBufferString(splunkOutputLines)
+	var splunkUrl = strings.Trim(os.Getenv("SPLUNK_URL"), " ") // Trim leading and trailing spaces, if present
 
 	// Splunk provides x509: certificate signed by unknown authority :-( , so we need to skip those checks (like curl -k)
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 	client := &http.Client{Transport: tr}
-	// Selectively copy across request method and headers
 
+	// Selectively copy across request method and headers
 	req, _ := http.NewRequest(session.HTTPMethod, splunkUrl, buf)
 	for hname, hval := range session.Headers {
 		switch hname {
@@ -94,10 +100,23 @@ func rest_webhooks_post(session events.APIGatewayProxyRequest) (events.APIGatewa
 	if err != nil {
 		log.Fatal(err)
 	}
-	buf2 := new(bytes.Buffer)
-	buf2.ReadFrom(res.Body)
+
+	resBuffer := new(bytes.Buffer)
+	resBuffer.ReadFrom(res.Body)
+	resStr := resBuffer.String()
+	// Special case: SparkPost sent an empty "ping" on webhook creation. Splunk returns Response 400 {"text":"No data","code":5}
+	var splunkRes splunkResult
+	if err := json.Unmarshal([]byte(resStr), &splunkRes); err != nil {
+		log.Fatal(err)
+	}
+	if splunkOutputLines == "" && res.StatusCode >= 400 && splunkRes.Code == 5 && splunkRes.Text == "No data" {
+		resStr = fmt.Sprintf("[response %d %s mapped -> 200 OK by this adapter]", res.StatusCode, resStr)
+		res.StatusCode = 200
+	}
+	log.Printf("Outgoing: %s %s: Response %d %s", req.Method, splunkUrl, res.StatusCode, resStr)
+
 	return events.APIGatewayProxyResponse{
-		Body:       buf2.String(),
+		Body:       resStr,
 		StatusCode: res.StatusCode,
 	}, nil
 }
@@ -116,10 +135,7 @@ func main() {
 			var req events.APIGatewayProxyRequest
 			err = json.Unmarshal(b, &req)
 
-			var res events.APIGatewayProxyResponse
-			res, _ = Handler(req)
-			fmt.Println("Status code:\t", res.StatusCode)
-			fmt.Println("Body:\t", res.Body)
+			_, _ = Handler(req)
 		}
 	} else {
 		// runtime code
